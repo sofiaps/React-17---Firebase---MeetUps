4b43e2f003fee8c84ba56963590e75475dcd7998
sofiaps <31549943+sofiaps@users.noreply.github.com>
2023-01-31 09:44:13 +0100

    Add files via upload

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+
+import MeetupList from '../components/meetups/MeetupList';
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://react-redux-cart-d3644-default-rtdb.europe-west1.firebasedatabase.app/meetups.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+      {/* {loadedMeetups.length ? <MeetupList meetups={loadedMeetups} /> : (<span>No Meetups available</span>)} */}
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
